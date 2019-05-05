327ef73fe9d2b4bc0dada35b3f7d4e53938847ae
zhouxiaohu <419049297@qq.com>
2019-05-05 08:40:57 +0800

    init

---
+++ michi-server/src/main/java/com/xiaohuzhou/Application.java
@@ -0,0 +1,17 @@
+package com.xiaohuzhou;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ * @Auther: ZhouXiaoHu
+ * @Date: 2019/5/3
+ * @Description:
+ */
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(RandomStringUtils.randomAlphanumeric(32));
+    }
+
+}
